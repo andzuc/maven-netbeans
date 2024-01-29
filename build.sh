a4086f95bce9efad838a3cf0418d21627ee28c00
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-01-29 20:13:26 +0100

    Netbeans working!

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 [ -f screenlog.0 ]&&rm screenlog.0
-screen -L sh -c "tar -C context -cf - .|docker build --progress=plain -t andzuc/alpine-netbeans -"
+screen -L sh -c "tar -C context -cf - .|docker build --progress=plain -t andzuc/maven-netbeans -"
--- runbash.sh
+++ runbash.sh
@@ -5,5 +5,5 @@ export XAUTH0="$(xauth nlist :0)"
 docker run --rm -it --net=host \
        --env="DISPLAY" \
        --env="XAUTH0" \
-       andzuc/alpine-netbeans \
+       andzuc/maven-netbeans \
        /bin/bash

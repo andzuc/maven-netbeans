73f8f91d86422032f9ba157f89f7ff2feb859b85
Andrea Zuccherelli <andrea@zuccherelli.net>
2024-02-01 23:51:46 +0100

    /tmp/.X11-unix/X0 to guest

--- netbeans.sh
+++ netbeans.sh
@@ -2,10 +2,11 @@
 
 export XAUTH0="$(xauth nlist ${DISPLAY})"
 
-docker run --rm -d --net=host \
+docker run --rm -d \
        --name netbeans \
        --env="DISPLAY" \
        --env="XAUTH0" \
+       -v "/tmp/.X11-unix/X0:/tmp/.X11-unix/X0" \
        -v maven_config:/home/netbeans/.m2 \
        -v netbeans_config:/home/netbeans/.netbeans \
        -v "${HOME}/dev:/home/netbeans/dev" \
--- runcmd.sh
+++ runcmd.sh
@@ -2,11 +2,12 @@
 
 export XAUTH0="$(xauth nlist ${DISPLAY})"
 
-docker run --rm -it --net=host \
+docker run --rm -it \
        --name runcmd.sh \
        --env="DISPLAY" \
        --env="XAUTH0" \
        --entrypoint /bin/bash \
+       -v "/tmp/.X11-unix/X0:/tmp/.X11-unix/X0" \
        -v maven_config:/home/netbeans/.m2 \
        -v netbeans_config:/home/netbeans/.netbeans \
        -v "${HOME}/dev:/home/netbeans/dev" \
